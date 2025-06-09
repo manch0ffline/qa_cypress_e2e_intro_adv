c9ea02b91a3f78399336d2e1c741ac2beb7fe8ac
Roman Romanchuk <roma.roman4uk.48@gmail.com>
2025-06-09 21:06:31 +0200

    refactor: switch to ES module syntax in config

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('cypress');
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
     baseUrl: 'https://conduit.mate.academy',
     setupNodeEvents(on, config) {
